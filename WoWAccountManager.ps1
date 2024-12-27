2371b8405caadb8329c8074999caa515579ccd6a
s0f1st1kt3dB3ar <49764053+dan4451@users.noreply.github.com>
2024-12-26 19:26:41 -0800

    fixed the startup from VanillaTweaks to just WoW.exe. I need to add support for the other exe's

--- WoWAccountManager.ps1
+++ WoWAccountManager.ps1
@@ -154,7 +154,7 @@ if (-not $account) {
 $AccountEncrypted = Get-Content "$env:USERPROFILE\Documents\WindowsPowerShell\Scripts\encrypted\$account" | ConvertTo-SecureString
 $credential = New-Object System.Management.Automation.PSCredential ($account.BaseName, $AccountEncrypted)
 # Launch Application
-Start-Process "C:\WoW 1.12\World of Warcraft 1.12\WoW.exe"
+Start-Process "$wowLocation" -PassThru
 
 # Wait for application to launch
 Start-Sleep -Seconds 5
